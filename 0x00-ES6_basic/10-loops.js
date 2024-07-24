87ab0fbed23ee7f4f32ed1c91058239d7e7deb20
Firaol Mk <firaolmekbib511@gmail.com>
2024-07-24 15:24:26 +0300

    loops and for...of

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
+  for (const idx of array) {
+    result.push(appendString + idx);
+  }
+  return result;
+}
