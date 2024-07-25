1b2c7ff7b9ecd6ed408f6a9b63afbdee7c10d6ec
Firaol Mk <firaolmekbib511@gmail.com>
2024-07-25 13:28:26 +0300

    iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,13 @@
+/* eslint-disable radix */
 export default function iterateThroughObject(reportWithIterator) {
-    let result = '';
+  let result = '';
 
-    for (const [idx, item] of Object.entries(reportWithIterator)) {
-        result += `${item}`;
+  for (const [idx, item] of Object.entries(reportWithIterator)) {
+    result += `${item}`;
 
-        if (parseInt(idx) !== reportWithIterator.length - 1) {
-            result += ' | ';
-        }
+    if (parseInt(idx) !== reportWithIterator.length - 1) {
+      result += ' | ';
     }
-    return result;
+  }
+  return result;
 }
