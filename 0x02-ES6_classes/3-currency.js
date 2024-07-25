a1642157a217b1ee3bf9c440e57513e9810a35f6
Firaol Mk <firaolmekbib511@gmail.com>
2024-07-26 01:50:04 +0300

    currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (typeof val !== 'string') throw new TypeError('code must be a string');
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('code must be a string');
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    return this._name(this._code);
+  }
+}
