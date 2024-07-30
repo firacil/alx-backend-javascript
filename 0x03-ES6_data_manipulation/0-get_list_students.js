699b08aa62c6380f54c29028cf8989b43aa2337e
Firaol Mk <firaolmekbib511@gmail.com>
2024-07-30 23:06:41 +0300

    updated

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const template = [{ id: 1, firstName: 'Guillame', Location: 'San Franscisco' },
+    { id: 2, firstName: 'James', Location: 'Columbia' },
+    { id: 5, firstName: 'Serena', Location: 'San Franscisco' },
+  ];
+  return template;
+}
