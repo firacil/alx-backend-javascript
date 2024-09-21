27593bc2943e04d95d733d1e4d4233f4e7c62aa5
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-21 20:42:31 +0000

    updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,8 +7,8 @@ if (process.stdin.isTTY) {
   });
 } else {
   process.stdin.on('data', (data) => {
-	  process.stdout.write(`Your name is: ${data.toString()}`);
-	  process.exit();
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
   });
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
